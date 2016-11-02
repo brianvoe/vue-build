589f4e61a6e57c5a601c146910dbc26ac898eb97
Brianvoe <brian@webiswhatido.com>
2016-11-01 21:06:16 -0500

    help - file update.

--- bin/help.js
+++ bin/help.js
@@ -3,6 +3,13 @@
 /* eslint-env node */
 
 function help () {
-  console.error('Usage: vue-build [version|build|test]')
+  var log = console.log
+  log('Usage: vue-build <command>')
+  log('')
+  log('  dev - Starts a dev server')
+  log('  build - Starts a dev server')
+  log('  test - Run tests')
+  log('  lint - Lint files')
+  log('  version - Prints the package version')
 }
 help()
