2a48a4d6c1421be04de2e97307955c2f37f594d7
bvoelker <bvoelker@digitalh2o.com>
2016-11-11 16:01:27 -0600

    init - added webpack config file.

--- bin/init.js
+++ bin/init.js
@@ -6,6 +6,7 @@ module.exports = function (yargs) {
 
   var inquirer = require('inquirer')
   inquirer.prompt([
+    {type: 'confirm', name: 'webpackFile', message: 'Create/Override webpack file?', default: true},
     {type: 'confirm', name: 'envFile', message: 'Create/Override env file?', default: true},
     {type: 'confirm', name: 'eslintFile', message: 'Create/Override eslint file?', default: true},
     {type: 'confirm', name: 'sourceFolder', message: 'Create/Override /src folder?', default: true},
@@ -16,12 +17,17 @@ module.exports = function (yargs) {
 
     // .env file
     if (answers.envFile) {
+      console.log('Creating webpack file')
+      fs.copy(vueBuildRoot + 'webpack.config.js', projectRoot + '/webpack.config.js', function (err) {
+        if (err) { console.error(err); process.exit(1) }
+      })
+    }
+
+    // .env file
+    if (answers.envFile) {
       console.log('Creating env file')
       fs.copy(vueBuildRoot + '.env', projectRoot + '/.env', function (err) {
-        if (err) {
-          console.error(err)
-          process.exit(1)
-        }
+        if (err) { console.error(err); process.exit(1) }
       })
     }
 
@@ -29,10 +35,7 @@ module.exports = function (yargs) {
     if (answers.eslintFile) {
       console.log('Creating eslint file')
       fs.copy(vueBuildRoot + '.eslintrc', projectRoot + '/.eslintrc', function (err) {
-        if (err) {
-          console.error(err)
-          process.exit(1)
-        }
+        if (err) { console.error(err); process.exit(1) }
       })
     }
 
@@ -40,20 +43,14 @@ module.exports = function (yargs) {
     if (answers.sourceFolder) {
       console.log('Creating source folder')
       fs.copy(vueBuildRoot + 'src', projectRoot + '/src', function (err) {
-        if (err) {
-          console.error(err)
-          process.exit(1)
-        }
+        if (err) { console.error(err); process.exit(1) }
       })
     }
     // source file
     if (answers.testFolder) {
       console.log('Creating test folder')
       fs.copy(vueBuildRoot + 'test', projectRoot + '/test', function (err) {
-        if (err) {
-          console.error(err)
-          process.exit(1)
-        }
+        if (err) { console.error(err); process.exit(1) }
       })
     }
   })
