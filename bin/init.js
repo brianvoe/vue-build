1155f0d985d30ef6fda1eb79e2e9230885b3e9ea
bvoelker <bvoelker@digitalh2o.com>
2016-11-11 15:47:59 -0600

    init - updated init file with folder and file copying

--- bin/init.js
+++ bin/init.js
@@ -9,13 +9,15 @@ module.exports = function (yargs) {
 
   var inquirer = require('inquirer')
   inquirer.prompt([
-    {type: 'confirm', name: 'envFile', message: 'Create env file?', default: true},
-    {type: 'confirm', name: 'eslintFile', message: 'Create eslint file?', default: true},
-    {type: 'confirm', name: 'sourceFolder', message: 'Create /src folder?', default: true},
-    {type: 'confirm', name: 'testFolder', message: 'Create /test folder?', default: true}
+    {type: 'confirm', name: 'envFile', message: 'Create/Override env file?', default: true},
+    {type: 'confirm', name: 'eslintFile', message: 'Create/Override eslint file?', default: true},
+    {type: 'confirm', name: 'sourceFolder', message: 'Create/Override /src folder?', default: true},
+    {type: 'confirm', name: 'testFolder', message: 'Create/Override /test folder?', default: true}
   ])
   .then(function (answers) {
     console.log() // Console Spacing
+
+    // .env file
     if (answers.envFile) {
       console.log('Creating env file')
       fs.copy(vueBuildRoot + '.env', projectRoot + '/.env', function (err) {
@@ -23,8 +25,38 @@ module.exports = function (yargs) {
           console.error(err)
           process.exit(1)
         }
+      })
+    }
 
-        console.log('success')
+    // eslint file
+    if (answers.eslintFile) {
+      console.log('Creating eslint file')
+      fs.copy(vueBuildRoot + '.eslintrc', projectRoot + '/.eslintrc', function (err) {
+        if (err) {
+          console.error(err)
+          process.exit(1)
+        }
+      })
+    }
+
+    // source file
+    if (answers.sourceFolder) {
+      console.log('Creating source folder')
+      fs.copy(vueBuildRoot + 'src', projectRoot + '/src', function (err) {
+        if (err) {
+          console.error(err)
+          process.exit(1)
+        }
+      })
+    }
+    // source file
+    if (answers.testFolder) {
+      console.log('Creating test folder')
+      fs.copy(vueBuildRoot + 'test', projectRoot + '/test', function (err) {
+        if (err) {
+          console.error(err)
+          process.exit(1)
+        }
       })
     }
   })
