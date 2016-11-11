9b382516376ee79acbeb4478be60f315b7f436ca
bvoelker <bvoelker@digitalh2o.com>
2016-11-11 11:03:04 -0600

    init - added init file with copying

--- bin/init.js
+++ bin/init.js
@@ -1,3 +1,21 @@
 module.exports = function (yargs) {
-  console.log('Initiate')
+  var copy = require('copy')
+  // var vueBuildRoot = '../'
+  var projectRoot = process.cwd()
+
+  var inquirer = require('inquirer')
+  inquirer.prompt([
+    {type: 'confirm', name: 'envFile', message: 'Create env file?', default: true},
+    {type: 'confirm', name: 'eslintFile', message: 'Create eslint file?', default: true},
+    {type: 'confirm', name: 'sourceFolder', message: 'Create /src folder?', default: true},
+    {type: 'confirm', name: 'testFolder', message: 'Create /test folder?', default: true}
+  ])
+  .then(function (answers) {
+    console.log(answers)
+    if (answers.envFile) {
+      copy('.env', projectRoot, function (err, files) {
+        if (err) { throw err }
+      })
+    }
+  })
 }
