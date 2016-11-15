c9c083600e817329a26c44482a48ba6c3a678480
Brianvoe <brian@webiswhatido.com>
2016-11-14 22:44:10 -0600

    e2e - updated comments

--- bin/test_e2e.js
+++ bin/test_e2e.js
@@ -1,20 +1,17 @@
 module.exports = function (yargs) {
-  // 1. start the dev server
+  // Set env variables
   process.env.NODE_ENV = 'testing'
   process.env.ENVIRONMENT = 'testing'
+
+  // Start the dev server
   var server = require('./dev.js')(yargs)
   var path = require('path')
 
-  // 2. run the nightwatch test suite against it
-  // to run in additional browsers:
-  //    1. add an entry in test/e2e/nightwatch.conf.json under "test_settings"
-  //    2. add it to the --env flag below
-  // or override the environment flag, for example: `npm run e2e -- --env chrome,firefox`
-  // For more information on Nightwatch's config file, see
-  // http://nightwatchjs.org/guide#settings-file
+  // Put together nightwatch options
   var opts = []
   opts = opts.concat(['--config', path.join(__dirname, 'config/nightwatch.conf.js')])
 
+  // Run nightwatch
   var spawn = require('cross-spawn')
   var runner = spawn('./node_modules/.bin/nightwatch', opts, { stdio: 'inherit' })
 
