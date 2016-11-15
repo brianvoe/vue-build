7cd4a15be7f153aa392209fcd1ac4047e1452142
Brianvoe <brian@webiswhatido.com>
2016-11-14 22:46:57 -0600

    lint - added linting

--- bin/lint.js
+++ bin/lint.js
@@ -1,3 +1,16 @@
+#!/usr/bin/env node
+
+/* eslint-env node */
+
 module.exports = function (yargs) {
-  console.log('Run your linting here')
+  var spawn = require('cross-spawn')
+  var opts = [
+    '--config', process.cwd() + '/.eslintrc', // Config
+    '--ext', '.js,.vue', // Extensions
+    process.cwd() + '/src',
+    process.cwd() + '/test/unit',
+    process.cwd() + '/test/e2e/spec'
+  ]
+
+  spawn('./node_modules/.bin/eslint', opts, { stdio: 'inherit' })
 }
