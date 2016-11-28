7d69095e790b3f3513cd730832417b35ee630842
bvoelker <bvoelker@digitalh2o.com>
2016-11-28 13:22:46 -0600

    test - changed back file name.

--- test/unit/specs/index.js
+++ test/unit/specs/index.js
@@ -5,12 +5,8 @@ describe('Simple assertion test', function () {
   it('Make sure test runs', function (done) {
     const vm = new Vue(app).$mount()
 
-    let five = 6
-    if (five === 5) {
-      console.log('hello')
-    }
-
-    vm.message = 'foo'
+    // vm.message = 'foo'
+    vm.updateMessage('foo')
     // wait a "tick" after state change before asserting DOM updates
     Vue.nextTick(() => {
       assert.equal(vm.message, 'foo', 'message doesnt equal foo')
