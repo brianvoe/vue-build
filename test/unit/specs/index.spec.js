063217cd2e175a323d843234ffda404dc5381504
bvoelker <bvoelker@digitalh2o.com>
2016-11-28 12:00:51 -0600

    test unit - updated file to be index.spec.js

--- test/unit/specs/index.spec.js
+++ test/unit/specs/index.spec.js
@@ -5,6 +5,11 @@ describe('Simple assertion test', function () {
   it('Make sure test runs', function (done) {
     const vm = new Vue(app).$mount()
 
+    let five = 6
+    if (five === 5) {
+      console.log('hello')
+    }
+
     vm.message = 'foo'
     // wait a "tick" after state change before asserting DOM updates
     Vue.nextTick(() => {
