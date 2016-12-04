7a6afcd9f1a693d9738c0f7e002f80e40f12fc5e
Brianvoe <brian@webiswhatido.com>
2016-12-04 02:36:16 -0600

    unit testing - added output for unit testing

--- bin/test_unit.js
+++ bin/test_unit.js
@@ -30,6 +30,11 @@ exports.handler = function (yargs) {
   process.env.SINGLE_RUN = yargs['single-run']
 
   var path = require('path')
+  var chalk = require('chalk')
+
+  // Ouput whats going on
+  console.log(chalk.blue('Building src files for tests'))
+
   // Start karma server
   var Server = require('karma').Server
   new Server({
