21feda12d46e3b9090e87dbd8fa03611e3c90a2b
bvoelker <bvoelker@digitalh2o.com>
2016-12-06 15:14:50 -0600

    unit test - added test for simple fetch

--- test/unit/specs/index.js
+++ test/unit/specs/index.js
@@ -11,4 +11,15 @@ describe('Simple assertion test', function () {
     // Via return promise make sure message equals what it should
     return assert.equal(vm.message, 'foo Now go build something!', 'message doesnt equal foo')
   })
+
+  it('Make server request', function () {
+    return fetch('http://localhost:' + process.env.PORT + '/ok')
+    .then(function (response) {
+      return response.text()
+    }).then(function (text) {
+      assert.equal(text, 'ok', 'response from server does not match')
+    }).catch(function (err) {
+      throw new Error(err.message)
+    })
+  })
 })
