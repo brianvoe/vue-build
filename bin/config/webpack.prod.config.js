fade467e2492e3a92a25ee1a672bba2432fd4e11
bvoelker <bvoelker@digitalh2o.com>
2016-12-08 10:57:48 -0600

    webpack production - added check for whether or not you have a index.html file. Not needed if you are bundleing a package or something

--- bin/config/webpack.prod.config.js
+++ bin/config/webpack.prod.config.js
@@ -5,6 +5,8 @@ var BabiliMinifyPlugin = require('babili-webpack-plugin')
 var baseWebpackConfig = require('./webpack.base.config.js')
 var ProgressBarPlugin = require('progress-bar-webpack-plugin')
 var chalk = require('chalk')
+var fs = require('fs')
+var projectRoot = process.cwd()
 
 process.env.NODE_ENV = process.env.environment
 
@@ -21,18 +23,26 @@ var webpackConfig = merge(baseWebpackConfig, {
     new BabiliMinifyPlugin({
       comments: false,
       sourceMape: process.env.SOURCE_MAP
-    }),
-    new HtmlWebpackPlugin({
-      inject: true,
-      showErrors: false,
-      template: 'index.html',
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeAttributeQuotes: true
-      }
     })
   ]
 })
+
+// Check if they have an html file to output if so add plugin
+try {
+  var indexHtml = projectRoot + '/src/index.html'
+  fs.statSync(indexHtml)
+
+  // If your here it does add plugin
+  webpackConfig.plugins.push(new HtmlWebpackPlugin({
+    inject: true,
+    showErrors: false,
+    template: 'index.html',
+    minify: {
+      removeComments: true,
+      collapseWhitespace: true,
+      removeAttributeQuotes: true
+    }
+  }))
+} catch (err) {}
 
 module.exports = webpackConfig
