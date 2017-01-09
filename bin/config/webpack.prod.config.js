25b429b2213432027ebea6dd861e8a05d6c1bf10
bvoelker <bvoelker@digitalh2o.com>
2017-01-09 11:45:18 -0600

    webpack - integrated css extraction

--- bin/config/webpack.prod.config.js
+++ bin/config/webpack.prod.config.js
@@ -1,5 +1,6 @@
 var webpack = require('webpack')
 var merge = require('webpack-merge')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var BabiliMinifyPlugin = require('babili-webpack-plugin')
 var baseWebpackConfig = require('./webpack.base.config.js')
@@ -27,12 +28,24 @@ var webpackConfig = merge(baseWebpackConfig, {
   ]
 })
 
+// Add css extraction
+var extractCSS = new ExtractTextPlugin('[name].css')
+for (var rule in webpackConfig.module.rules) {
+  if (webpackConfig.module.rules[rule].test.test('.css')) {
+    // Replace default loader with extractCSS
+    webpackConfig.module.rules[rule].loader = extractCSS.extract(['css-loader?sourceMap', 'sass-loader?sourceMap'])
+
+    // Add extraction plugin
+    webpackConfig.plugins.push(extractCSS)
+  }
+}
+
 // Check if they have an html file to output if so add plugin
 try {
   var indexHtml = projectRoot + '/src/index.html'
   fs.statSync(indexHtml)
 
-  // If your here it does add plugin
+  // If your here it did find an html file and now we add plugin
   webpackConfig.plugins.push(new HtmlWebpackPlugin({
     inject: true,
     showErrors: false,
