b3abf4f179cbfe5e86edbe554cb1ad306b557e2a
Brianvoe <brian@webiswhatido.com>
2017-02-06 00:05:31 -0600

    docs - work in progress

---
+++ docs/src/app.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import app from 'src/app.vue'
+
+import 'src/style'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: (h) => h(app)
+})
---
+++ docs/src/app.vue
@@ -0,0 +1,24 @@
+<script>
+  import logo from './logo.png'
+  export default {
+    data: function () {
+      return {
+        logo
+      }
+    },
+    methods: {}
+  }
+</script>
+
+<template>
+  <div class="app">
+    <div class="header">
+      <div class="logo"><img :src="logo" />ue-build</div>
+      <div class="tagline">Taking the frustrating build process and clutter out of your application</div>
+    </div>
+    <div class="container">
+      <div class="sidebar">Sidebar</div>
+      <div class="main">Main section</div>
+    </div>
+  </div>
+</template>
