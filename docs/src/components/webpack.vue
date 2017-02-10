a6a9d9bdfe06c10f89e778d30216ba21ad6be59c
Brianvoe <brian@webiswhatido.com>
2017-02-10 00:31:03 -0600

    docs - text updates

--- docs/src/components/webpack.vue
+++ docs/src/components/webpack.vue
@@ -2,8 +2,8 @@
   <div>
     <h1>Webpack</h1>
     <p>
-      By default vue-build has a base config file, which you can see here.
-      So its not necessary to add webpack.config.js file unless you want/need to.
+      By default vue-build has a base config file.
+      So its not necessary to add webpack.config.js file unless you want/need to make webpack alterations.
     </p>
   </div>
 </template>
