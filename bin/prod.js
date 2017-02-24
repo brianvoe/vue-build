ce9a76456ae1a517b94440c661b58ebaf78b6b0a
brianvoe <brian@webiswhatido.com>
2017-02-24 14:53:57 -0600

    production - adjusted to make css extraction optional

--- bin/prod.js
+++ bin/prod.js
@@ -1,13 +1,48 @@
 #!/usr/bin/env node
-
 /* eslint-env node */
 
-module.exports = function (yargs) {
+// Sub options for prod command
+exports.builder = {
+  e: {
+    alias: 'css-extraction',
+    type: 'boolean',
+    default: true,
+    describe: 'extract css'
+  }
+}
+
+// prod command function
+exports.handler = function (yargs) {
   var rimraf = require('rimraf')
   rimraf(process.cwd() + '/dist', require('fs'), function (info) {})
 
   var webpack = require('webpack')
   var config = require('./config/webpack.prod.config.js')
+  var ExtractTextPlugin = require('extract-text-webpack-plugin')
+  var cssExtraction = yargs['css-extraction']
+
+  // Add css extraction
+  if (cssExtraction) {
+    var extractCSS = new ExtractTextPlugin({
+      filename: '[name].css',
+      disable: false,
+      allChunks: true
+    })
+    for (var rule in config.module.rules) {
+      if (config.module.rules[rule].test.test('.css')) {
+      // Replace default loader with extractCSS
+        config.module.rules[rule].loader = extractCSS.extract([
+          'css-loader?sourceMap',
+          'resolve-url-loader',
+          'sass-loader?sourceMap'
+        ])
+
+      // Add extraction plugin
+        config.plugins.push(extractCSS)
+      }
+    }
+  }
+  console.log(config.plugins)
 
   // Run webpack
   webpack(config, function (err, stats) {
