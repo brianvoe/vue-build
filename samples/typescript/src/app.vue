8f6e6e504fcc1afa95c179aa2818c03cce193da6
Brianvoe <brian@webiswhatido.com>
2017-03-30 00:04:09 -0500

    typescript - work in progress

---
+++ samples/typescript/src/app.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import Vue, { ComponentOptions } from 'vue'
+
+  export default {
+    data () {
+      return {
+        info: 'Hello World'
+      }
+    },
+  } as ComponentOptions<Vue>
+</script>
+
+<template>
+  <div>{{info}}</div>
+</template>
